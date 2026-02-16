06e62901537e266540e76129008ad048acccbe82
Tamilselvan-ats <ts4490797@gmail.com>
2026-02-16 14:23:00 +0530

    commit successful

--- OOPS.java
+++ OOPS.java
@@ -1,5 +1,5 @@
 
-public class Oops {
+public class OOPS{
     public static void main(String[] args) {
 
         System.out.println(" *****    *****    *****    ***** ");
@@ -12,6 +12,7 @@ public class Oops {
 
     }
 }
+
 
 
 
