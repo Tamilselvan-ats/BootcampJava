9fed4d868651573189eb5d25ce59912a0c9c4376
Tamilselvan-ats <ts4490797@gmail.com>
2026-02-16 14:03:40 +0530

    commit successful

--- OOPS.java
+++ OOPS.java
@@ -1,17 +1,18 @@
-
 public class Oops {
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *****    *****  ");
-        System.out.println("*     *  *     *  *             * ");
-        System.out.println("*     *  *     *  *             * ");
-        System.out.println(" *****    *****    *       *****  ");
+        String output = String.join("\n",
+            " *****    *****    *****    ***** ",
+            "*     *  *     *  *     *  *     *",
+            "*     *  *     *  *     *  *      ",
+            "*     *  *     *  *****    *****  ",
+            "*     *  *     *  *             * ",
+            "*     *  *     *  *             * ",
+            " *****    *****    *       *****  "
+        );
+
+        System.out.println(output);
 
     }
 }
-
-
 
